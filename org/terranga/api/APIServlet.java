5b27629577a08777ee0e4542e865686c22657646
bkarpinos <bkarpinos@gmail.com>
2015-06-22 15:48:40 -0400

    add post call for reviews

--- org/terranga/api/APIServlet.java
+++ org/terranga/api/APIServlet.java
@@ -770,6 +770,30 @@ public class APIServlet extends HttpServlet {
 			}
 		}
 		
+		if (resource.equals("reviews")){
+			String body = getBody(req);
+			
+			try{
+				JSONObject json = new JSONObject(body);
+				Review review = new Review();
+				review.update(json);
+				review.save();
+				
+				response.put("confirmation", "success");
+				response.put("review", review.getSummary()); 
+				JSONObject jsonResponse = new JSONObject(response);
+				resp.getWriter().print(jsonResponse.toString());
+				return;
+			}
+			catch(JSONException e){
+				response.put("confirmation", "fail");
+				response.put("message", e.getMessage());
+				JSONObject jsonResponse = new JSONObject(response);
+				resp.getWriter().print(jsonResponse.toString());
+				return;
+			}
+		}
+		
 	}
 	
 	
