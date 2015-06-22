0b5ebcd41810e8d99ec536b05818ee5af262a13c
bkarpinos <bkarpinos@gmail.com>
2015-06-22 15:57:32 -0400

    begin bug fixes for reviews

--- org/terranga/api/APIServlet.java
+++ org/terranga/api/APIServlet.java
@@ -72,7 +72,8 @@ public class APIServlet extends HttpServlet {
 	        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 	        //Endorsement.fetchEndorsements(datastore, 0);
 //	        Insight.fetchInsightsWithProfileID(datastore, "123123", 0);	  
-	        Endorsement.fetchEndorsementsWithEndorsed(datastore, "1", 0);
+	        //Endorsement.fetchEndorsementsWithEndorsed(datastore, "1", 0);
+	        Review.fetchReviewsWithReviewed(datastore, "1", 0);
 		}
 		
 		
